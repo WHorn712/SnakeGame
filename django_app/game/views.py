1d250164f3e1f1e12ad4c8866f3f31fe3c526c9c
Wellington Augusto Horn <wellingtondeveloper31@gmail.com>
2024-10-03 12:52:35 -0300

    1 3-10-2024

--- django_app/game/views.py
+++ django_app/game/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import logica_negocio.snake_game as sg
 
 # Create your views here.
+
+def snake_game_view(request):
+    game = {'Display': sg.Display(),
+            'Score': sg.Score(),
+            'Snake': sg.Snake(),
+            'Food': sg.Food()}
+    return render(request, 'snake/game.html', {'game': game})
